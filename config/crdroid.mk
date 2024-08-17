dfce1254e8d1d5ca8435134a004c89793958a930
SirRGB <sirrgb@proton.me>
2024-08-17 21:50:21 +0200

    Bump version to MAY26
    * https://review.lineageos.org/q/topic:R_asb_2026-05
    
    Signed-off-by: SirRGB <sirrgb@proton.me>

--- config/crdroid.mk
+++ config/crdroid.mk
@@ -1,5 +1,5 @@
 # Unofficial patch level
-PRODUCT_SYSTEM_DEFAULT_PROPERTIES += ro.lineage.custom_version=2026-04-01
+PRODUCT_SYSTEM_DEFAULT_PROPERTIES += ro.lineage.custom_version=2026-05-01
 
 # Additional props
 PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
